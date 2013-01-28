67e928b15cd5cbc1c37a2d9d436e224705327933
Jonas Baumann <jone@jone.ch>
2013-01-28 10:59:44 +0100

    Add missing plone version classifiers.

--- setup.py
+++ setup.py
@@ -12,25 +12,35 @@ setup(name='collective.lastmodifier',
       description="Extends Plone content with metadata about the last modifier",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
         "Framework :: Plone",
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
         "Programming Language :: Python",
         ],
+
       keywords='',
       author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/collective.lastmodifier',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
           'setuptools',
           'collective.monkeypatcher',
       ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
