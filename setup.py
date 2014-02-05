c11c130fe8fc9a5412961809e9d03e1e3daf5657
Jonas Baumann <jone@jone.ch>
2014-02-05 09:24:54 +0100

    4teamwork is an AG.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='collective.lastmodifier',
         ],
 
       keywords='',
-      author='4teamwork GmbH',
+      author='4teamwork AG',
       author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/collective.lastmodifier',
       license='GPL2',
